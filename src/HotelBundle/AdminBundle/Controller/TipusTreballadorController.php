88b2dfc142dd186e240acb392a8de3b6890faad3
Unknown <csosac@gmail.com>
2017-04-25 15:33:28 +0200

    Acabat tipus Treballador

--- src/HotelBundle/AdminBundle/Controller/TipusTreballadorController.php
+++ src/HotelBundle/AdminBundle/Controller/TipusTreballadorController.php
@@ -12,9 +12,11 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class TipusTreballadorController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('HotelBundle:Default:backend.html.twig');
+    public function indexAction(){
+        $tipusTreb = $this->getDoctrine()->getRepository('HotelBundle:TipusTreballador')->findAll();
+        return $this->render('HotelBundleAdminBundle:TipusTreballador:llista.html.twig', array(
+                    'array' => $tipusTreb
+        ));
     }
 
     public function addTipusTreballadorAction(Request $request)
@@ -36,12 +38,89 @@ class TipusTreballadorController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $rol->setDescripcio($form->get('rol')->getData());           
             $tipusTreballador->setDescripcio($form->get('descripcio')->getData());
-            $rol->setDescripcio($form->get('rol')->getData());
+            $tipusTreballador->setRolAsociat($rol);
 
             $em = $this->getDoctrine()->getManager();
-            $em->persist($tipusTreballador);
             $em->persist($rol);
+            $em->persist($tipusTreballador);
+            $em->flush();
+
+            return $this->render('HotelBundleAdminBundle:Default:objectAdded.html.twig', array(
+            'titol' => 'Nou Tipus de treball afegit'));
+        };
+ 
+        return $this->render('HotelBundleAdminBundle:Default:addObject.html.twig', array(
+            'titol' => 'Afegir tipus de Treball',
+            'form' => $form->createView()
+        ));
+    }
+
+    public function editTipusTreballadorAction(Request $request)
+    {
+        $tipusTreballador = new TipusTreballador();
+        $rol = new Rol();
+ 
+        $form = $this->createFormBuilder()
+            ->add('descripcio', TextType::class, array('label' => 'Descripció','attr' => array(
+                    'class' => 'form-control'),
+                    'label_attr'=> array('class' => 'label_text spaceTop')))
+            ->add('rol', TextType::class, array('label' => 'Rol','attr' => array(
+                    'class' => 'form-control'),
+                    'label_attr'=> array('class' => 'label_text spaceTop')))
+            ->add('save', SubmitType::class, array('label' => 'Crear Tipus Treballador' ,'attr' => array(
+                        'class' => 'btn btn-primary spaceTop')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $rol->setDescripcio($form->get('rol')->getData());           
+            $tipusTreballador->setDescripcio($form->get('descripcio')->getData());
+            $tipusTreballador->setRolAsociat($rol);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rol);
+            $em->persist($tipusTreballador);
+            $em->flush();
+
+            return $this->render('HotelBundleAdminBundle:Default:objectAdded.html.twig', array(
+            'titol' => 'Nou Tipus de treball afegit'));
+        };
+ 
+        return $this->render('HotelBundleAdminBundle:Default:addObject.html.twig', array(
+            'titol' => 'Afegir tipus de Treball',
+            'form' => $form->createView()
+        ));
+    }
+
+    public function deleteTipusTreballadorAction(Request $request)
+    {
+        $tipusTreballador = new TipusTreballador();
+        $rol = new Rol();
+ 
+        $form = $this->createFormBuilder()
+            ->add('descripcio', TextType::class, array('label' => 'Descripció','attr' => array(
+                    'class' => 'form-control'),
+                    'label_attr'=> array('class' => 'label_text spaceTop')))
+            ->add('rol', TextType::class, array('label' => 'Rol','attr' => array(
+                    'class' => 'form-control'),
+                    'label_attr'=> array('class' => 'label_text spaceTop')))
+            ->add('save', SubmitType::class, array('label' => 'Crear Tipus Treballador' ,'attr' => array(
+                        'class' => 'btn btn-primary spaceTop')))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $rol->setDescripcio($form->get('rol')->getData());           
+            $tipusTreballador->setDescripcio($form->get('descripcio')->getData());
+            $tipusTreballador->setRolAsociat($rol);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($rol);
+            $em->persist($tipusTreballador);
             $em->flush();
 
             return $this->render('HotelBundleAdminBundle:Default:objectAdded.html.twig', array(
