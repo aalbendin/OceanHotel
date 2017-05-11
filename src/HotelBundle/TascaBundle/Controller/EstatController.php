a3786e46f5bc548ae4d409f0fc665fa62296fae2
Unknown <csosac@gmail.com>
2017-05-11 15:33:42 +0200

    Estat tasca CRUD

--- src/HotelBundle/TascaBundle/Controller/EstatController.php
+++ src/HotelBundle/TascaBundle/Controller/EstatController.php
@@ -4,36 +4,30 @@ namespace HotelBundle\TascaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use HotelBundle\Entity\TipusTasca;
+use HotelBundle\Entity\Estat;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class TipusTascaController extends Controller
+class EstatController extends Controller
 {
     public function indexAction(){
-        $tipusTasca = $this->getDoctrine()->getRepository('HotelBundle:TipusTasca')->findAll();
+        $estats = $this->getDoctrine()->getRepository('HotelBundle:Estat')->findAll();
 
-        return $this->render('HotelBundleTascaBundle:Default:tipusTasca.html.twig', array(
-                    'array' => $tipusTasca
+        return $this->render('HotelBundleTascaBundle:Default:estat.html.twig', array(
+                    'array' => $estats
         ));
     }
 
-    public function addTipusTascaAction(Request $request)
+    public function addEstatAction(Request $request)
     {
-        $tipusTasca = new TipusTasca();
+        $estat = new Estat();
  
-        $form = $this->createFormBuilder($tipusTasca)
+        $form = $this->createFormBuilder($estat)
             ->add('descripcio', TextType::class, array('label' => 'Descripció','attr' => array(
                     'class' => 'form-control'),
                     'label_attr'=> array('class' => 'label_text spaceTop')))   
-            ->add('tipusTreballador',  EntityType::class, array(
-                'class' => 'HotelBundle:TipusTreballador',
-                'choice_label' => 'descripcio',
-                'multiple' => FALSE,
-                'label_attr'=> array('class' => 'label_text spaceTop'), 
-                'attr' => array('class' => 'form-control')))    
-            ->add('save', SubmitType::class, array('label' => 'Crear Tipus d\'Habitació',
+            ->add('save', SubmitType::class, array('label' => 'Crear Estat',
                     'attr' => array(
                         'class' => 'btn btn-warning mt')))
             ->getForm();
@@ -42,38 +36,32 @@ class TipusTascaController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($tipusTasca);
+            $em->persist($estat);
             $em->flush();
 
             $this->get('session')->getFlashBag()->add(
                     'notice',array(
                     'type' => 'success',
-                    'msg' => 'S\'ha afegit el Tipus de tasca'
+                    'msg' => 'S\'ha afegit l\'Estat'
             ));            
-            return $this->redirect($this->generateurl('hotel_bundle_tasca_homepage'));
+            return $this->redirect($this->generateurl('hotel_bundle_estat_homepage'));
         };
  
         return $this->render('HotelBundleAdminBundle:Default:addObject.html.twig', array(
-            'titol' => 'Afegir tipus tasca',
+            'titol' => 'Afegir Estat',
             'form' => $form->createView()
         ));
     }
 
-    public function editTipusTascaAction($id,Request $request)
+    public function editEstatAction($id,Request $request)
     {
-        $TipusTasca = $this->getDoctrine()->getRepository('HotelBundle:TipusTasca')->findOneById($id);
+        $estat = $this->getDoctrine()->getRepository('HotelBundle:Estat')->findOneById($id);
  
-        $form = $this->createFormBuilder($tipusTasca)
+        $form = $this->createFormBuilder($estat)
             ->add('descripcio', TextType::class, array('label' => 'Descripció','attr' => array(
                     'class' => 'form-control'),
-                    'label_attr'=> array('class' => 'label_text spaceTop')))   
-            ->add('tipusTreballador',  EntityType::class, array(
-                'class' => 'HotelBundle:TipusTreballador',
-                'choice_label' => 'descripcio',
-                'multiple' => FALSE,
-                'label_attr'=> array('class' => 'label_text spaceTop'), 
-                'attr' => array('class' => 'form-control')))    
-            ->add('save', SubmitType::class, array('label' => 'Editar Tipus d\'Habitació',
+                    'label_attr'=> array('class' => 'label_text spaceTop'))) 
+            ->add('save', SubmitType::class, array('label' => 'Editat Estat',
                     'attr' => array(
                         'class' => 'btn btn-warning mt')))
             ->getForm();
@@ -82,15 +70,15 @@ class TipusTascaController extends Controller
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
-            $em->persist($TipusTasca);
+            $em->persist($estat);
             $em->flush();
 
             $this->get('session')->getFlashBag()->add(
                     'notice',array(
                     'type' => 'success',
-                    'msg' => 'S\'ha editat el Tipus de tasca'
+                    'msg' => 'S\'ha editat l\'estat'
             ));
-            return $this->redirect($this->generateurl('hotel_bundle_tasca_homepage'));
+            return $this->redirect($this->generateurl('hotel_bundle_estat_homepage'));
         };
  
         return $this->render('HotelBundleAdminBundle:Default:addObject.html.twig', array(
@@ -99,26 +87,26 @@ class TipusTascaController extends Controller
         ));
     }
 
-    public function deleteTipusTascaAction($id){
-        $tipusTasca = $this->getDoctrine()->getRepository('HotelBundle:TipusTasca')->findOneById($id);
+    public function deleteEstatAction($id){
+        $estat = $this->getDoctrine()->getRepository('HotelBundle:Estat')->findOneById($id);
 
-        if ($tipusTasca != null) {
+        if ($estat != null) {
             $em = $this->getDoctrine()->getManager();
-            $em->remove($tipusTasca);
+            $em->remove($estat);
             $em->flush();
             $this->get('session')->getFlashBag()->add(
                     'notice',array(
                     'type' => 'success',
-                    'msg' => 'S\'ha eliminat el tipus de tasca'
+                    'msg' => 'S\'ha eliminat l\'estat'
             ));
         }else{
             $this->get('session')->getFlashBag()->add(
                     'notice',array(
                     'type' => 'danger',
-                    'msg' => 'No s\'ha eliminat el tipus de tasca'
+                    'msg' => 'No s\'ha eliminat l\'estat'
             ));
         }
-        return $this->redirect($this->generateurl('hotel_bundle_tasca_homepage'));
+        return $this->redirect($this->generateurl('hotel_bundle_estat_homepage'));
     }
 
 }
