d0b715b611c3530b025e7f7836f66cc98c840090
Unknown <csosac@gmail.com>
2017-05-15 15:28:05 +0200

    Repositorios y arreglos

--- src/HotelBundle/TascaBundle/Controller/TascaController.php
+++ src/HotelBundle/TascaBundle/Controller/TascaController.php
@@ -15,35 +15,15 @@ class TascaController extends Controller
     public function indexAction(){
       $container= $this->container;
       $em = $this->getDoctrine()->getManager();
-      $treb = $em->getRepository('HotelBundle:Treballador');
-      $treballador = $treb->retornaTreballador($container);
-      $tasques = $this->getTasques($treballador);
+      $trebRepo = $em->getRepository('HotelBundle:Treballador');
+      $treballador = $trebRepo->retornaTreballador($container);
 
-        return $this->render('HotelBundleTascaBundle:Default:llistaTreballadorTasca.html.twig', array(
+      $tascaRepository = $em->getRepository('HotelBundle:Tasca');
+      $tasques = $tascaRepository->getTasquesByTreballadorType($treballador);
+
+        return $this->render('HotelBundleTascaBundle:Default:llistaTreballadorsTasca.html.twig', array(
                     'array' => $tasques
         ));
     }
 
-    public function getTasques(){
-         $em = $this->getDoctrine()->getManager();
-    $query = $em->createQuery(
-    'SELECT p.id
-    FROM HotelBundle:Tasca p
-    WHERE p.estatId = 1 and p.tipusTasca NOT IN
-    (SELECT r
-     FROM HotelBundle:TipusTasca r
-     where r.id not in 
-     (select d
-      from Hotelbundle:TipusTreballador d
-      where d.id = :tipusTreballador
-         )
-    )'
-    )->setParameter('tipusTreballador', $tipusTreballador->getId());
-
-    $array= array();
-    foreach ($query as $key => $value) {
-        array_push($array, $value);
-    }
-    return $array;
-    }
 }
